e48f9bbf47a131db11b3869cdcb112753d38312b
Raf <rafaelmarianorocha@gmail.com>
2019-07-21 15:18:24 -0700

    Add some styling to the videoList component.

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,18 @@
 import React from "react";
+import "./VideoItem.css";
 
 const VideoItem = ({ video }) => {
     return (
-        <div>
-            <img alt="video" src={video.snippet.thumbnails.medium.url} />
-            {video.snippet.title}
+        <div className="video-item item">
+            <img
+                className="ui image"
+                alt="video"
+                src={video.snippet.thumbnails.medium.url}
+            />
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+                <div>{video.snippet.description}</div>
+            </div>
         </div>
     );
 };
